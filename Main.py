39780a8c49d5f4b3e42b282e9dfd6c0288092d3d
Mahdi Ghaznavi <ghaznavi.mahdi@gmail.com>
2020-08-03 01:05:30 +0430

    main configured.

--- Main.py
+++ Main.py
@@ -1,14 +1,21 @@
 from HttpServer import HttpServer
+from HttpProxyServer import HttpProxyServer
 import atexit
 
 if __name__ == "__main__":
     httpServer = HttpServer(8080)
+    proxy_server = HttpProxyServer(8090)
     try:
         # TODO start http proxy server
+        proxy_server.setDaemon(True)
+        proxy_server.start()
+        atexit.register(proxy_server.stop)
+
         httpServer.setDaemon(True)
         httpServer.start()
         atexit.register(httpServer.stop)
         httpServer.join()
+        proxy_server.join()
     except KeyboardInterrupt:
         pass
     finally:
